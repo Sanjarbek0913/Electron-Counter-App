a663b63f904f26ae510d0bea0cdbe5c139a572c1
Sanjarbek <sanjarbekabdurayimov4@gmail.com>
2024-04-03 15:09:52 +0500

    Commit for Electron Counter

--- ElectronCounter/ViewController.swift
+++ ElectronCounter/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var plusBtn: UIButton!
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var minusBtn: UIButton!
+    
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func plusPressed(_ sender: Any) {
+        if count == 33 {
+            let AudioURL = NSURL(fileURLWithPath: Bundle.main.path(forResource: "note", ofType: "mp3")!)
+            var AudioPlayer = AVAudioPlayer()
+
+            do {
+                AudioPlayer = try AVAudioPlayer(contentsOf: AudioURL as URL)
+            } catch _ as NSError {
+                fatalError()
+            }
+
+            AudioPlayer.play()
+            count = 0
+            countLabel.text = "\(count)"
+        } else {
+            count += 1
+            countLabel.text = "\(count)"
+        }
+        
+    }
+    
+    
+    @IBAction func minusPressed(_ sender: Any) {
+        count -= 1
+        if count >= 0 {
+            countLabel.text = "\(count)"
+        } else {
+            count = 0
+        }
+    }
+  
+    
+    @IBAction func resetPressed(_ sender: Any) {
+        count = 0
+        countLabel.text = "\(count)"
+    }
+    
+    
 }
 
